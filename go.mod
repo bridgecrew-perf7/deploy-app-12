46ff6272075019d4788718155fad18808f88185a
Juan Diego Echeverri Lopez <j.diego2200@gmail.com>
2021-05-24 23:39:40 -0500

    !

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/JuanDiegoE/deploy-app
 go 1.16
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"path"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 )
 
@@ -13,7 +15,12 @@ func main() {
 	server := echo.New()
 	server.GET(path.Join("/"), Version)
 
-	server.Start(":3000")
+	godotenv.Load()
+	port := os.Getenv("PORT")
+
+	address := fmt.Sprintf("%s:%s", "0.0.0.0", port)
+	fmt.Println(address)
+	server.Start(address)
 
 }
 func Version(context echo.Context) error {
